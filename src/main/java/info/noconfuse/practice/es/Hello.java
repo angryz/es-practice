4d782d92b29d6ced65f56232c120ebfa6a0ca77e
angryz <angryzorro@gmail.com>
2016-09-14 19:57:42 +0800

    practice search api

--- src/main/java/info/noconfuse/practice/es/Hello.java
+++ src/main/java/info/noconfuse/practice/es/Hello.java
@@ -2,13 +2,19 @@ package info.noconfuse.practice.es;
 
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.search.SearchHit;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -110,6 +116,60 @@ public class Hello {
             System.out.println("_type: " + response.getType());
             System.out.println("_id: " + response.getId());
             System.out.println("_version: " + response.getVersion());
+
+            client.close();
+        }
+    }
+
+    private static class MultiGetApi {
+
+        public static void main(String[] args) throws Exception {
+            Client client = getClient();
+
+            MultiGetResponse response = client.prepareMultiGet()
+                    .add("us", "tweet", "4", "6", "12")
+                    .add("us", "user", "1")
+                    .add("megacrop", "employee", "1")
+                    .get();
+
+            for (MultiGetItemResponse item : response) {
+                GetResponse resp = item.getResponse();
+                if (resp.isExists()) {
+                    System.out.println("_index: " + resp.getIndex());
+                    System.out.println("_type: " + resp.getType());
+                    System.out.println("_id: " + resp.getId());
+                    System.out.println("_version: " + resp.getVersion());
+                    System.out.println("_source: " + resp.getSourceAsString());
+                }
+            }
+
+            client.close();
+        }
+    }
+
+    private static class SearchApi {
+
+        public static void main(String[] args) throws Exception {
+            Client client = getClient();
+
+            SearchResponse response = client.prepareSearch("us", "megacrop")
+                    .setTypes("tweet", "employee")
+                    .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                    .setQuery(QueryBuilders.termQuery("about", "love"))
+                    .setPostFilter(QueryBuilders.rangeQuery("age").from(25).to(35))
+                    .setFrom(0)
+                    .setSize(20)
+                    .setExplain(true)
+                    .execute()
+                    .actionGet();
+            System.out.println("total: " + response.getHits().totalHits());
+            System.out.println("maxScore: " + response.getHits().maxScore());
+            System.out.println("took: " + response.getTookInMillis());
+            for (SearchHit hit : response.getHits().hits()) {
+                System.out.println("_index: " + hit.getIndex() + ", _type: " + hit.getType()
+                        + ", _id: " + hit.getId());
+                System.out.println("_source: " + hit.getSourceAsString());
+            }
 
             client.close();
         }
